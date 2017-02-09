ca289b881ff9f80cfe2b23fd185434f53db3e539
Nahuel <nahueldolores@hotmail.com>
2017-02-08 22:36:57 -0300

    Adjusted a few things.
    Don't forget to register the command on the plugin.yml, I don't want to create another PR just for that ;P

--- src/nl/jessegeerts/customplugins/voidtp/Main.java
+++ src/nl/jessegeerts/customplugins/voidtp/Main.java
@@ -16,6 +16,11 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by Jesse on 8-2-2017.
  */
 public class Main extends JavaPlugin implements Listener{
+    
+    private Location spawn;
+    
+    //This is just to don't write getConfig() over and over again.
+    private FileConfiguration config;
 
     public void onEnable(){
         getLogger().info("This plugin was made by Jesse Geerts");
@@ -27,28 +32,57 @@ public class Main extends JavaPlugin implements Listener{
         getConfig().addDefault("Pitch", 0);
         getConfig().addDefault("Yaw", 0);
         saveConfig();
+        
+        config = getConfig();
+        loadLocation();
     }
 
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event){
-        Player p = event.getPlayer();
-        Location loc = p.getLocation();
-        World world = Bukkit.getServer().getWorld(getConfig().getString("World"));
-
+        Player player = event.getPlayer();
+        Location loc = player.getLocation();
+        
         if (loc.getBlockY() <= 1){
-            p.teleport(new Location(world, getConfig().getInt("X"), getConfig().getInt("Y"), getConfig().getInt("Z"), getConfig().getInt("Pitch"), getConfig().getInt("Yaw")));
+        	if (!player.getWorld().equals(spawn.getWorld())) return;
             event.setCancelled(true);
+            player.teleport(spawn);
         }
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandlabel, String[] args){
         if(cmd.getName().equalsIgnoreCase("voidtpreload")){
             reloadConfig();
+            loadLocation();
             sender.sendMessage(ChatColor.GREEN + "Config has been reloaded");
         }
-
+        else if (cmd.getName().equalsIgnoreCase("setvoidspawn")) {
+            if (!(sender instanceof Player)) sender.sendMessage(ChatColor.RED + "You need to be a Player in order to do this!");
+            else {
+                Player player = (Player) sender;   
+                
+                Location location = player.getLocation();
+                spawn = location;
+                
+                config.set("World", location.getWorld().getName());
+                config.set("X", (int) location.getX());
+                config.set("Y", (int) location.getY());
+                config.set("Z", (int) location.getZ());
+                config.set("Pitch", (int) location.getPitch());
+                config.set("Yaw", (int) location.getYaw());
+                saveConfig();
+                
+                player.sendMessage(ChatColor.GREEN + "Spawn successfully set!");
+            }
+        }
 
         return true;
+    }
+    
+    private void loadLocation() {
+        World world = Bukkit.getWorld(config.getString("World"));
+        spawn = new Location(world, config.getInt("X"), config.getInt("Y"), config.getInt("Z"));
+        spawn.setYaw((float) config.getDouble("Yaw"));
+        spawn.setPitch((float) config.getDouble("Pitch"));
     }
 }
