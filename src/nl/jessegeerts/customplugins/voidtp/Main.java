c6b942566aee16054696b4669cb46801470b8294
Nahuel <nahueldolores@hotmail.com>
2017-02-08 22:39:19 -0300

    Fixed imports

--- src/nl/jessegeerts/customplugins/voidtp/Main.java
+++ src/nl/jessegeerts/customplugins/voidtp/Main.java
@@ -6,6 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
